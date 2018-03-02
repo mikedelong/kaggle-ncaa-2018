d68f4a5882487624f81d2a59a6aecdcecab02f00
mikedelong <mikedelong@outlook.com>
2018-03-02 12:00:06 -0500

    feature: check file input files, make sure they're all there, and load them. Quit if any of them is missing.

--- code/advanced_analytics.py
+++ code/advanced_analytics.py
@@ -2,7 +2,10 @@
 # https://www.kaggle.com/virtonos/advanced-basketball-analytics
 
 import logging
+import os
 import time
+
+import pandas as pd
 
 start_time = time.time()
 
@@ -14,6 +17,29 @@ console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 console_handler.setLevel(logging.DEBUG)
 logger.debug('started')
+
+input_files = sorted([
+    '../input/NCAATourneyCompactResults.csv',
+    '../input/RegularSeasonDetailedResults.csv',
+    '../input/Teams.csv',
+    '../input/NCAATourneySeeds.csv',
+    '../input/Conferences.csv',
+    '../input/MasseyOrdinals.csv',
+    '../input/SampleSubmissionStage1.csv'
+])
+for input_file in input_files:
+    if os.path.isfile(input_file):
+        logger.debug('input file %s exists' % input_file)
+    else:
+        logger.warning('input file %s is missing. Quitting.' % input_file)
+
+df_tourney = pd.read_csv('../input/NCAATourneyCompactResults.csv')
+df_season = pd.read_csv('../input/RegularSeasonDetailedResults.csv')
+df_teams = pd.read_csv('../input/Teams.csv')
+df_conferences = pd.read_csv('../input/Conferences.csv')
+df_rankings = pd.read_csv('../input/MasseyOrdinals.csv')
+df_sample_sub = pd.read_csv('../input/SampleSubmissionStage1.csv')
+df_seeds = pd.read_csv('../input/NCAATourneySeeds.csv')
 
 logger.debug('done')
 finish_time = time.time()
