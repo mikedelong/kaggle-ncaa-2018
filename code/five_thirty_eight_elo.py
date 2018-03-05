4c9d2486743dc371ac9559161e42afaae430c634
mikedelong <mikedelong@outlook.com>
2018-03-05 16:08:36 -0500

    feature: logging and timing; no functionality yet

---
+++ code/five_thirty_eight_elo.py
@@ -0,0 +1,24 @@
+# credit where credit is due
+# https://www.kaggle.com/.../fivethirtyeight-elo-ratings
+
+import logging
+import time
+
+start_time = time.time()
+
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+default_head = 10
+
+logger.debug('done')
+finish_time = time.time()
+elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+logger.info("Time: {:0>2}:{:0>2}:{:05.2f}".format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
